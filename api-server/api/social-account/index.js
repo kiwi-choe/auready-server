9d9d02ff956d6b0b8d17e039b19a317b6af5efba
choekiwi <magmag0221@gmail.com>
2016-11-01 13:32:33 +0900

    When register social account, set social-account email value to userinfo-email property

--- api-server/api/social-account/index.js
+++ api-server/api/social-account/index.js
@@ -5,7 +5,7 @@ const oauthInfo = require('./oauth-info');
 
 var User = require(__appbase_dirname + '/models/user');
 
-const temp_id_token = 'eyJhbGciOiJSUzI1NiIsImtpZCI6IjczZTMzNmE2ZmMwYTk1MDFjMzBiNGRhOGZlMDM4OGM2NjZjYWQ2YjQifQ.eyJpc3MiOiJodHRwczovL2FjY291bnRzLmdvb2dsZS5jb20iLCJpYXQiOjE0Nzc3MTY3MjcsImV4cCI6MTQ3NzcyMDMyNywiYXVkIjoiODUxMTQwNjAxNDYwLWZxYnIyNnJqa29yOWFiNTU3bDZsNHZybWJnM2JoNGdpLmFwcHMuZ29vZ2xldXNlcmNvbnRlbnQuY29tIiwic3ViIjoiMTA3NjQ4NzgxMjExMjczNjQ1MDIyIiwiZW1haWxfdmVyaWZpZWQiOnRydWUsImF6cCI6Ijg1MTE0MDYwMTQ2MC00a3FqZjBsM3MyZ2ozb2RlYm0xYXYwdWUyOW5jbDhvNy5hcHBzLmdvb2dsZXVzZXJjb250ZW50LmNvbSIsImVtYWlsIjoibWFnbWFnMDIyMUBnbWFpbC5jb20iLCJuYW1lIjoi7LWc7KeA7JuQIiwicGljdHVyZSI6Imh0dHBzOi8vbGg0Lmdvb2dsZXVzZXJjb250ZW50LmNvbS8tWFBOMzVPVVVLLUkvQUFBQUFBQUFBQUkvQUFBQUFBQUFBQUEvQUtUYWVLOXoyMXFvLUdLYUpvUU9lbnhsNXI4UEV4ajVlUS9zOTYtYy9waG90by5qcGciLCJnaXZlbl9uYW1lIjoi7KeA7JuQIiwiZmFtaWx5X25hbWUiOiLstZwiLCJsb2NhbGUiOiJrbyJ9.ZK3yPWoH9XSvb4SjT1VL3cKsPZQG-p17mpodXASzgg_ymbwibCL8HzS3QkOKwcT5mKsWmh6ar6SCdTp30BwQ7vw3q_Zbnb2nd97CDHCkrZtKG6_BErnHCRrXTGA0WP53zI5cZE04l2vDjlG5XmkrqSM79KmzKj-hikzSxAK0CEqoOzVVbZr00GpDhmxR4XKDtufej4oXk9uutz5PMoGr3smd_ARcpAS5ogrlz4hJ6v-K804Eryaglm9ebRbDRi7wXJmsDFz9ICjdpgvHuPgRsp_VrMKQcbK-bxNgjdjL0KuxoRZ8dUwIzHVbJvROHXddZt5rwBy62sX4oAKrRMoppQ';
+const temp_id_token = 'eyJhbGciOiJSUzI1NiIsImtpZCI6ImE4OGVjMjM3ZDYzMmJiMTJiMDgxNzRjNjY4Y2RkMmMzOGNlNzJiNWIifQ.eyJpc3MiOiJodHRwczovL2FjY291bnRzLmdvb2dsZS5jb20iLCJpYXQiOjE0Nzc5NzQ0NTIsImV4cCI6MTQ3Nzk3ODA1MiwiYXVkIjoiODUxMTQwNjAxNDYwLWZxYnIyNnJqa29yOWFiNTU3bDZsNHZybWJnM2JoNGdpLmFwcHMuZ29vZ2xldXNlcmNvbnRlbnQuY29tIiwic3ViIjoiMTA3NjQ4NzgxMjExMjczNjQ1MDIyIiwiZW1haWxfdmVyaWZpZWQiOnRydWUsImF6cCI6Ijg1MTE0MDYwMTQ2MC00a3FqZjBsM3MyZ2ozb2RlYm0xYXYwdWUyOW5jbDhvNy5hcHBzLmdvb2dsZXVzZXJjb250ZW50LmNvbSIsImVtYWlsIjoibWFnbWFnMDIyMUBnbWFpbC5jb20iLCJuYW1lIjoi7LWc7KeA7JuQIiwicGljdHVyZSI6Imh0dHBzOi8vbGg0Lmdvb2dsZXVzZXJjb250ZW50LmNvbS8tWFBOMzVPVVVLLUkvQUFBQUFBQUFBQUkvQUFBQUFBQUFBQUEvQUtUYWVLOXoyMXFvLUdLYUpvUU9lbnhsNXI4UEV4ajVlUS9zOTYtYy9waG90by5qcGciLCJnaXZlbl9uYW1lIjoi7KeA7JuQIiwiZmFtaWx5X25hbWUiOiLstZwiLCJsb2NhbGUiOiJrbyJ9.b0FYTPsku35aXrR-13_kAvvcX2kvHW6yJjTIDFfud7Hsp1jthrZ0tRxyuBiTWDuwAXZhrHhrwGH2HVdqiDW4I3KlYzqfD2giq3SHLWa7xkoCFLSVs-OBv6TWHvZodowIVuM57dJmP4xxOpGJP2Ji1w1HcZnMUe_nTU0O3tKQ-RQ4F4kWf0FCVtMoN8JtZT13IoO4hfnswmSkLlHhSh66_HAsr1KSWNi94e-NzxweSef4fLB7Y0_KB4bv4MaBNwK3OerIgo9wWis1oYRDkwULN21S-gerhYTu1UI8RgnKoat1Woi90wZH1E5PqbPLY2v6Sng0rpVK71XtHXgv44RyZw';
 
 router.post('/login/:socialapp', (req, res) => {
 
@@ -43,7 +43,7 @@ router.post('/login/:socialapp', (req, res) => {
             console.log(tokenInfo);
             let userinfo = {
                 name: tokenInfo.name,
-                email: req.params.socialapp,
+                email: tokenInfo.email,
                 password: req.body.id_token
             };
             registerSocialAccount(userinfo, done => {
