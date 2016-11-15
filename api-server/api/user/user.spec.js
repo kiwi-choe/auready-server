2760c2002a25f9d21b2f34e5bdea060222b7cbbf
choekiwi <magmag0221@gmail.com>
2016-11-15 12:57:11 +0900

    Add test database in user.spec

--- api-server/api/user/user.spec.js
+++ api-server/api/user/user.spec.js
@@ -1,28 +1,49 @@
-const should = require('should');
+process.env.dbURI = 'test';
 
+const should = require('should');
 const server = require('../../../www');
 const request = require('supertest')(server);
 
-const accessToken = 'QjxAmH8WEhwOQsXVFMV99SM3MjNZN284YDls5310fMB4DzLWe6nr5xv9nVLwPEUHBfzcOJ54QhiCjprK5G7EF973OVDhPZjJBvaLsNjeVPbKeMXobUtwCN1OcVY2xH7yzWLiTpWGcVQalja6bStMdDfHPU4UXpMR7LQkgJtHWSB6Olr5bGhhrFmWdt6py0UsgwXKOEvgXZrzADG6MGiZ4cEukv0D8ITWPGbiKW0JmSsscgUbtItDVdnKyfk4Ubgl';
+const Token = require('../../../models/token.controller');
+const predefine = require('../../../auth-server/util/predefine');
+const clientId = 'tEYQAFiAAmLrS2Dl';
 
-const config = {
-
-    headerForApi: {
-        Authorization: 'Bearer' + ' ' + accessToken
-    },
-};
+const User = require('../../../models/user.controller');
+const test_name = 'nameofkiwi3';
+const test_email = 'kiwi3@gmail.com';
+const test_password = '123';
 
 const search = 'kiwi';
 describe('GET /user/:search', () => {
+
+    let accessToken;
+    before(done => {
+        // Register user first
+        User.create(test_name, test_email, test_password, true, (err, user, info) => {
+            // Add Token
+            Token.create(clientId, user.id, predefine.oauth2.type.password, (err, newToken) => {
+                accessToken = newToken.accessToken;
+                done();
+            });
+        });
+    });
+    after(done => {
+        // delete all the users
+        User.deleteAll(err => {
+            Token.deleteAll(err => {
+                done();
+            });
+        });
+    });
+
+
     it('GET /user/:search - should return 200 code', done => {
         request
             .get('/user/' + search)
-            .set(config.headerForApi)
+            .set({Authorization: 'Bearer' + ' ' + accessToken})
             .expect(200)
             .end((err, res) => {
                 if(err) throw err;
-                // res.body.should.have.property('email', 'kiwi');
-
                 done();
             });
     });
